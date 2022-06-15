769053a8df8e9d7f7087c077c78cb3e2ffc994e1
Deivinson Schmalbach <deivinson7059@gmail.com>
2022-06-15 10:11:05 -0500

    first commit

---
+++ schemaGenerator.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const tjs = require("typescript-json-schema");
+const fs = require("fs");
+
+const settings = {
+  required: true,
+  ref: false,
+};
+const compilerOptions = {
+  strictNullChecks: true,
+};
+
+const program = tjs.getProgramFromFiles([path.resolve("schema_definition.ts")], compilerOptions, "./");
+
+const schema = tjs.generateSchema(program, "*", settings);
+console.log(schema);
+
+fs.writeFileSync(
+  "_schema.ts",
+  "const schema = " + JSON.stringify(schema) + " as const;\nexport default schema.definitions;"
+);
